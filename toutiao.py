bd91faddcb16aff0130824c5e57a6a11bab90672
hans362 <i@hans362.cn>
2024-12-28 11:34:34 +0800

    Add toutiao analyzer

--- toutiao.py
+++ toutiao.py
@@ -1,6 +1,6 @@
 import json
 from matplotlib import pyplot as plt
-from datetime import datetime, timedelta
+from datetime import datetime
 from evaluators.induction import InductionEvaluator
 from evaluators.relevance import RelevanceEvaluator
 import os
@@ -37,13 +37,12 @@ def calculate_index_bulk(articles):
     return articles
 
 
-def calculate_average_index(articles, start_date, end_date):
+def calculate_average_index(articles):
     total_score = 0
     cnt = 0
     for article in articles:
-        if start_date <= article["update_time"] <= end_date:
-            total_score += article["score"]
-            cnt += 1
+        total_score += article["score"]
+        cnt += 1
     return total_score / cnt if cnt > 0 else 0
 
 
@@ -53,49 +52,39 @@ def evaluate_dataset(file, max_size=None):
 
     x = []
     y = []
-    days = 365 * 2
-    start_date = datetime.now() - timedelta(days=days)
     print("===================================")
     print(
         "Average index of "
         + os.path.basename(file)
-        + " in the past {} days".format(days)
         + ": "
-        + str(calculate_average_index(articles, start_date, datetime.now()))
-    )
-    print(
-        f"Average index in 2023: {calculate_average_index(articles, datetime(2023, 1, 1), datetime(2023, 12, 31))}"
-    )
-    print(
-        f"Average index in 2024: {calculate_average_index(articles, datetime(2024, 1, 1), datetime(2024, 12, 31))}"
+        + str(calculate_average_index(articles))
     )
     print("===================================")
-    for i in range(days - 120):
-        end_date = start_date + timedelta(days=120)
-        x.append(end_date)
-        y.append(calculate_average_index(articles, start_date, end_date))
-        start_date += timedelta(days=1)
+    for i in range(300 - 20):
+        end = i + 20
+        x.append(end)
+        y.append(calculate_average_index(articles[i:end]))
     plt.plot(x, y, label=os.path.basename(file).split(".")[0])
 
     articles = sorted(articles, key=lambda x: x["score"], reverse=True)
     print("===================================")
-    print("Top 20 articles")
+    print("Top 10 articles")
     print("===================================")
-    for article in articles[:20]:
+    for article in articles[:10]:
         print(article["title"] + "\t" + str(article["score"]))
     print("===================================")
-    print("Bottom 20 articles")
+    print("Bottom 10 articles")
     print("===================================")
-    for article in articles[-20:]:
+    for article in articles[-10:]:
         print(article["title"] + "\t" + str(article["score"]))
 
 
-corpus_dir = "./wechat-corpus"
+corpus_dir = "./toutiao-corpus"
 for file in os.listdir(corpus_dir):
     if file.endswith(".json"):
         evaluate_dataset(os.path.join(corpus_dir, file))
 
-plt.title("部分高校微信公众号平均标题党指数变化（120天滑动窗口）")
+plt.title("今日头条部分新闻媒体平均标题党指数变化（20篇滑动窗口）")
 plt.ylabel("标题党指数")
 plt.legend()
 plt.show()
